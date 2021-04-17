d94a12eee61487771adf835b0d2466a39c7b7958
cynthia.anyaeriuba <canyaeriuba@gmail.com>
2021-04-17 23:05:28 +0100

    app set-up

---
+++ app.rb
@@ -0,0 +1,9 @@
+require 'sinatra/base'
+
+class RPS < Sinatra::Base
+
+
+  get '/' do
+    'Hello World'
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+
 require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
@@ -5,7 +6,7 @@ require 'simplecov-console'
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
 
